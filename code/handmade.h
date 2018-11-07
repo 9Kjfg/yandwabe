e1aec379b19de47b4887b817b71f46034f42c02a
Alex Boot <redactae@gmail.com>
2018-11-07 12:47:54 +0200

    46th

--- code/handmade.h
+++ code/handmade.h
@@ -58,17 +58,29 @@ struct hero_bitmaps
 	loaded_bitmap Torso;
 };
 
+struct entity
+{
+	bool32 Exists;
+	tile_map_position P;
+	v2 dP;
+	uint32 FacingDirection;
+	real32 Width, Height;
+};
+
 struct game_state
 {
 	memory_arena WorldArena;
 	world *World;
 
+	// TODO: Should we allow spit-screen?
+	uint32 CameraFollowingEntityIndex;
 	tile_map_position CameraP;
-	tile_map_position PlayerP;
-	v2 dPlayerP;
+
+	uint32 PlayerIndexForController[ArrayCount(((game_input *)0)->Controllers)];
+	uint32 EntityCount;
+	entity Entities[10];
 
 	loaded_bitmap Backdrop;
-	uint32 HeroFacingDirection;
 	hero_bitmaps HeroBitmaps[4];
 	// TODO: Player state should be canonical position
 };
